22b8a5c9276945fa137ed3431408eb080a223256
Deniz Uzun <du1@wellesley.edu>
2021-07-05 16:37:38 -0700

    assig 7

--- src/Assignment07.jl
+++ src/Assignment07.jl
@@ -1,7 +1,8 @@
 module Assignment07
 
+using Base: stop_reading
 export normalizeDNA
-
+export composition(sequence)
 # # uncomment the following line if you intend to use BioSequences types
 # using BioSequences
 
@@ -12,18 +13,121 @@ Ensures that a sequence only contains valid bases
 (or `'N'` for unknown bases).
 Returns a String.
 """
-function normalizeDNA(seq)
-    seq = uppercase(string(seq))
-    for base in seq
-        # note: `N` indicates an unknown base
+function normalizeDNA(sequence)
+    sequence = uppercase(string(sequence))
+    for base in sequence # note: `N` indicates an unknown base
         occursin(base, "AGCTN") || error("invalid base $base")
     end
-    return seq # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
+    return sequence # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
 end
 
 
 # Your code here.
+#=function composition(sequence)
+    sequence = normalizeDNA(sequence) #make uppercase string, check invalid bases
+    a = c = g = t = 0 #sets all 4 variables to `0`
+    comps = Dict() #initialize empty Dictionary for bases
+    stopindex= length(sequence) #stops at length of sequence
+
+    for i in 1:stopindex #for loop: traverses each sequence?
+        for base in sequence  #tells what to look at: but does it recognize the bases?
+        comp = sequence[i:stopindex] #indexing sequence?
+        comp = normalizeDNA(comp)
+        println(comp)
+        if base == 'A'
+            comps['A'] = a = a + 1
+            println(comps['A'])
+        end
+        if base == 'G'
+            g = g + 1
+           end
+        if base == 'T'
+            t = t + 1 
+        end
+        if base == 'C'
+            c = c + 1 
+        end
+    end
+end=#
+function composition(sequence)
+    sequence = normalizeDNA(sequence) #make uppercase string, check invalid bases
+    #a = c = g = t = n= 0 #sets all 4 variables to `0`
+    comp = Dict()
+    for i in 1:length(sequence)
+        pos = sequence[i]
+        if haskey(comp, pos) == true
+            comp[pos] = comp[pos] + 1
+        else
+            comp[pos] = 1
+        end
+    end
+    return comp
+end
 # Don't forget to export your functions!
 
+function gc_content(sequence)
+    ng = 0
+    nc = 0
+    sequence = uppercase(sequence)
+    for i in 1:length(sequence)
+        if sequence[i] == 'G'
+            ng = ng + 1
+        elseif sequence[i] == 'C'
+            nc = nc + 1
+        end
+    end
+    return (ng + nc) / length(sequence)
+end
+
+function complement(sequence)
+    sequence = uppercase(sequence)
+    ret = ""
+    for i in 1:length(sequence)
+        if sequence[i] == 'A'
+            ret = ret * 'T'
+        elseif sequence[i] == 'T'
+            ret = ret * 'A'
+        elseif sequence[i] == 'G'
+            ret = ret * 'C'
+        elseif sequence[i] == 'C'
+            ret = ret * 'G'
+        elseif sequence[i] == 'N'
+            ret = ret * 'N'
+        end
+    end
+    return ret 
+end
+
+function reverse_complement(sequence)
+    st= ""
+    sequence =reverse(sequence)
+    for i in 1:length(sequence)
+        st = st*string(complement(sequence[i]))
+    end
+    return st
+end
+
+function parse_fasta(path)
+    headers= []
+    sequences= []
+    tmp= ""
+    for line in eachline(path)
+        if startswith(line, '>')
+            if !isempty(tmp)
+                push!(sequences, tmp)
+            end
+            tmp= ""
+            push!(headers, SubString(line, 2))
+        else
+            for base in line # note: `N` indicates an unknown base
+                base = uppercase(base)
+                occursin(base, "AGCTN") || error("invalid base $base")
+            end
+            tmp= tmp*line
+        end
+    end
+        push!(sequences, tmp)
+    return (headers, sequences)
+end
 
 end # module Assignment07
